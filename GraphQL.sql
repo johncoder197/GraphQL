b1c1ae57fde99e39efccc2e80d9d83a10fa6e32f
johncoder197 <142368936+johncoder197@users.noreply.github.com>
2024-02-15 15:22:23 +0700

    Update GraphQL.sql

--- GraphQL.sql
+++ GraphQL.sql
@@ -1,7 +1,55 @@
-listRepos($queryString: String!) {
+query listRepos($queryString: String!){
     rateLimit{
      cost
      remaining
      resetAt
     }
-}
+    search(query:$queryString, type:REPOSITORY, first:20){  
+     repositoryCount
+     pageInfo{
+      endCursor
+      startCursor
+     }
+     edges{
+      node{
+       ... on Repository{
+        id
+        name
+        createdAt 
+        description 
+        isArchived
+        isPrivate
+        url
+        owner{
+         login
+         id
+         __typename
+         url
+        }
+        assignableUsers{
+         totalCount
+        }
+        licenseInfo{
+         key
+        }
+        defaultBranchRef{
+         target{
+          ... on Commit{
+           history(first:10){
+            totalCount
+            edges{
+             node{
+              ... on Commit{
+               committedDate
+              }
+             }
+            }
+           }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+   }

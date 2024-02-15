ef082f6af75a5c8ac5eb8aeb00bb6e961c83a08f
Johncoder <ducledoanminh1907@gmail.com>
2024-02-15 15:29:20 +0700

    Update

--- GraphQL.sql
+++ GraphQL.sql
@@ -1,7 +1,55 @@
-listRepos($queryString: String!) {
+query listRepos($queryString: String!){
     rateLimit{
      cost
      remaining
      resetAt
     }
-}
+    search(query:$queryString, type:REPOSITORY, first:20){  
+     repositoryCount
+     pageInfo{
+      endCursor
+      startCursor
+     }
+     edges{
+      node{
+       ... on Repository{
+        id
+        name
+        createdAt 
+        description 
+        isArchived
+        isPrivate
+        https://github.com/johncoder197/GraphQL.git
+        johncoder197{
+         login
+         id
+         __typename
+         url
+        }
+        assignableUsers{
+         totalCount
+        }
+        licenseInfo{
+         key
+        }
+        defaultBranchRef{
+         target{
+          ... on Commit{
+           history(first:10){
+            totalCount
+            edges{
+             node{
+              ... on Commit{
+               committedDate
+              }
+             }
+            }
+           }
+          }
+         }
+        }
+       }
+      }
+     }
+    }
+   }
